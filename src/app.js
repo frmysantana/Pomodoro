c31d9d972fa30daff0002d489b98650e459d1bf5
Fremy Santana <frmysantana@yahoo.com>
2018-02-11 22:14:04 -0500

    Delete app.js

--- src/app.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PomodoroClock from './Components/PomodoroClock';
-
-ReactDOM.render(<PomodoroClock />, document.getElementById('app'));
