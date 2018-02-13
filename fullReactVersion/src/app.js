71f0e7a57cf15a8fa0f63a38e2daa5d5313d56e5
Fremy Santana <frmysantana@yahoo.com>
2018-02-12 20:25:40 -0500

    Importing styles

--- fullReactVersion/src/app.js
+++ fullReactVersion/src/app.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PomodoroClock from './Components/PomodoroClock';
+import 'normalize.css/normalize.css';
+import './Styles/styles.scss';
 
 ReactDOM.render(<PomodoroClock />, document.getElementById('app'));
