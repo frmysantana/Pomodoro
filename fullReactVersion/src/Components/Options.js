0da634c27448438321b88f9b413683e810ee4c9f
Fremy Santana <frmysantana@yahoo.com>
2018-02-21 23:41:31 -0500

    Update Options.js

--- fullReactVersion/src/Components/Options.js
+++ fullReactVersion/src/Components/Options.js
@@ -10,20 +10,28 @@ export default class Options extends React.Component {
     changeSelected(e) {
       this.props.changeSelected(e.target.id);
     }
+
+    componentDidMount() {
+      document.querySelector('#session').checked = true;
+    }
     
     render() {
       return (
         <div id="Options">
-          <label htmlFor="session">Session Length</label>
-          <input 
-            type="radio" name="portion"
-            id="session" onClick={this.changeSelected}
-          />
+          <div>
+            <label htmlFor="session">Session <br /> Timer</label>
+            <input 
+              type="radio" name="portion"
+              id="session" onClick={this.changeSelected}
+            />
+          </div>
           
-          <label htmlFor="break">Break Length</label>
-          <input type="radio" name="portion" id="break" 
-            onClick={this.changeSelected}
-          />
+          <div>
+            <label htmlFor="break">Break <br /> Timer</label>
+            <input type="radio" name="portion" id="break" 
+              onClick={this.changeSelected}
+            />
+          </div>
         </div>
       );
     }
