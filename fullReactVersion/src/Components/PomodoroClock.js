a66251f81bec59188a3f5efae6fb72a1a2d9262a
Fremy Santana <frmysantana@yahoo.com>
2018-02-28 22:20:03 -0500

    Refactored the code that switched the displayed timer once the current timer had finished and alerts the user.

--- fullReactVersion/src/Components/PomodoroClock.js
+++ fullReactVersion/src/Components/PomodoroClock.js
@@ -7,6 +7,10 @@ import Adjust from './Adjust.js';
 export default class PomodoroClock extends React.Component {
   constructor(props) {
     super(props);
+    this.changeSelected = this.changeSelected.bind(this);
+    this.changeTime = this.changeTime.bind(this);
+    this.changeUnit = this.changeUnit.bind(this);
+    this.didTimeEnd = this.didTimeEnd.bind(this);
     this.incOrDec = [
       function(num) {return num + 1;},
       function(num) {return num - 1;}
@@ -15,14 +19,9 @@ export default class PomodoroClock extends React.Component {
       function(var1, var2) {return var1 < var2;},
       function(var1, var2) {return var1 > var2;}
     ];  
-    this.timer = {};
-    this.changeTime = this.changeTime.bind(this);
-    this.handleAdd = this.handleAdd.bind(this);
-    this.handleSub = this.handleSub.bind(this);
-    this.changeSelected = this.changeSelected.bind(this);
-    this.changeUnit = this.changeUnit.bind(this);
     this.startTimer = this.startTimer.bind(this);
     this.stopTimer = this.stopTimer.bind(this);
+    this.timer = {};
     this.state = {
       selected: props.selected,
       unit: props.unit,
@@ -40,7 +39,6 @@ export default class PomodoroClock extends React.Component {
       this.state.breakLength;
     const i = (mode === 'add') ? 0 : 1;
 
-    // console.log()
     min = parseInt(timeLength.split(':')[0]);
     sec = parseInt(timeLength.split(':')[1]);
 
@@ -75,169 +73,61 @@ export default class PomodoroClock extends React.Component {
     } else {
         this.setState(() => ({breakLength: min + ':' + sec}));
     }
+
+    if (this.timer.countDown) {this.didTimeEnd();}
   }
 
-  handleAdd() {
-    let min, sec, sel = this.state.selected, timerLimit;
-    timerLimit = (sel = 'session') ? timerLimit = 59: timerLimit= 29;
-
-    let {selected} = this.state;
-    console.log(selected);
-
-    if (sel === 'session') {
-      min = parseInt(this.state.sessionLength.split(':')[0]);
-      sec = parseInt(this.state.sessionLength.split(':')[1]);
-
-      
-      if (this.state.unit === "min") {
-        if (min < 59) {
-          min = min + 1;
-        } else {
-          alert('The maximum session time is 59:59.');
+  didTimeEnd() {
+    let message, i;
+    const activePar = document.querySelector('.active');
+    const switchObj = {
+        0: 1, 
+        1: 0
+      };
+  
+    // Switch timer and alert user
+    if (activePar.children[0].innerHTML === '0:00') {
+  
+      const pars = document.querySelectorAll('p');
+      pars.forEach(function (el, index) {
+        if (el === activePar) {
+          i = index;
         }
-      } else {
-        if (sec === 59 && min < 59) {
-          sec = 0;
-          min = min + 1;
-          sec = (sec < 10) ? '0' + sec : sec;
-        } else if (min + (sec/100) < 59.59) {
-          sec = sec + 1;
-          sec = (sec < 10) ? '0' + sec : sec;
-        } else {
-          alert('The maximum session time is 59:59.');
-        }
-      }
-    
-      this.setState(() => ({sessionLength: min + ':' + sec}));
-    } else {
-      min = parseInt(this.state.breakLength.split(':')[0]);
-      sec = parseInt(this.state.breakLength.split(':')[1]);
-      timerLimit = 29;
-      
-      if (this.state.unit === "min") {
-        if (min < 29) {
-          min = min + 1;
-        } else {
-          alert('The maximum break time is 29:59.');
-        }
-      } else {
-        if (sec === 59 && min < 29) {
-          sec = 0;
-          min = min + 1;
-          sec = (sec < 10) ? '0' + sec : sec;
-        } else if (min + (sec/100) < 29.59) {
-          sec = sec + 1;
-          sec = (sec < 10) ? '0' + sec : sec;
-        } else {
-          alert('The maximum break time is 29:59')
-        }
-      }
-    
-      this.setState(() => ({breakLength: min + ':' + sec}));
-    }
-  }
-    
-  handleSub() {
-    let min, sec, sel = this.state.selected;
-    
-    if (sel === 'session') {
-      min = parseInt(this.state.sessionLength.split(':')[0]);
-      sec = parseInt(this.state.sessionLength.split(':')[1]);
-      
-      if (this.state.unit === "min") {
-        if (min > 0) {
-          min = min - 1;
-        } else {
-          alert('The minimum session time is 5:00.');
-        }
-      } else {
-        if (sec === 0 && min > 0) {
-          sec = 59;
-          min = min - 1;
-          sec = (sec < 10) ? '0' + sec : sec;
-        } else if (min + (sec/100) > 0.00) {
-          sec = sec - 1;
-          sec = (sec < 10) ? '0' + sec : sec; 
-        } else {
-          alert('The minimum session time is 5:00.');
-          sec = (sec === 0) ? '0' + sec: sec;
-        }
-      }
-    
-      this.setState(() => ({sessionLength: min + ':' + sec}));
-      
-      if (this.timer.countDown && document.querySelectorAll('span')[0].innerHTML === '0:00') {
-        document.querySelectorAll('p')[0].classList.add('hide');
-        document.querySelectorAll('p')[0].classList.remove('active');
-
-        document.querySelectorAll('p')[1].classList.add('active');
-        document.querySelectorAll('p')[1].classList.remove('hide');
-
+      });
+  
+      // Show paragraph of timer that is about to start
+      document.querySelectorAll('p')[switchObj[i]].classList.add('active');
+      document.querySelectorAll('p')[switchObj[i]].classList.remove('hide');
+  
+      // Hide paragraph of timer that just finished
+      document.querySelectorAll('p')[i].classList.add('hide');
+      document.querySelectorAll('p')[i].classList.remove('active');
+  
+          
+      if (this.state.selected === 'session') {
         this.setState(() => ({
           selected: 'break',
           sessionLength: this.timer.defaultSession
         }));
-
-        setTimeout(function() {
-          const sound = new Howl({
-            src: ['./assets/Alarm.mp3', './assets/Alarm.ogg'],
-            onplay: function() {
-              alert('Session is over! Take a break.');
-              this.stop();
-            },
-          });
-          sound.play();
-        }, 1);
-      }
-    } else {
-      min = parseInt(this.state.breakLength.split(':')[0]);
-      sec = parseInt(this.state.breakLength.split(':')[1]);
-      
-      if (this.state.unit === "min") {
-        if (min > 0) {
-          min = min - 1;
-        } else {
-          alert('The minimum break time is 1:00.');
-        }
+        message = 'Session is over! Take a break.';
       } else {
-        if (sec === 0 && min > 0) {
-          sec = 59;
-          min = min - 1;
-          sec = (sec < 10) ? '0' + sec : sec;
-        } else if (min + (sec/100) > 0.00) {
-          sec = sec - 1;
-          sec = (sec < 10) ? '0' + sec : sec;
-        } else {
-          alert('The minimum break time is 1:00.');
-          sec = (sec === 0) ? '0' + sec : sec;
-        }
-      }
-    
-      this.setState(() => ({breakLength: min + ':' + sec}));
-      
-      if (this.timer.countDown && document.querySelectorAll('span')[1].innerHTML === '0:00') {
-        document.querySelectorAll('p')[0].classList.remove('hide');
-        document.querySelectorAll('p')[0].classList.add('active');
-
-        document.querySelectorAll('p')[1].classList.add('hide');
-        document.querySelectorAll('p')[1].classList.remove('active');
-        
         this.setState(() => ({
           selected: 'session',
           breakLength: this.timer.defaultBreak
         }));
-
-        setTimeout(function() {
-          const sound = new Howl({
-            src: ['./assets/Alarm.mp3', './assets/Alarm.ogg'],
-            onplay: function() {
-              alert('Break is over! Starting next session.');
-              this.stop();
-            },
-          });
-          sound.play();
-        }, 1);
+        message = 'Break is over! Starting next session.';
       }
+  
+      setTimeout(function() {
+        const sound = new Howl({
+          src: ['./assets/Alarm.mp3', './assets/Alarm.ogg'],
+          onplay: function() {
+                  alert(message);
+                  this.stop();
+              },
+        });
+        sound.play();
+      }, 1);
     }
   }
     
@@ -266,27 +156,29 @@ export default class PomodoroClock extends React.Component {
   }
     
   stopTimer() {
-    clearInterval(this.timer.countDown);
-    delete this.timer.countDown;
-    
-    document.querySelectorAll("input[type='radio']").forEach(function(el) {
-      el.disabled = false;
-    });
+    if (this.countDown) {
+      clearInterval(this.timer.countDown);
+      delete this.timer.countDown;
       
-    document.querySelectorAll(".js-adjust-time").forEach(function(el) {
-      el.disabled = false;
-    });
-    
-    document.querySelectorAll('p').forEach(function(el) {
-      el.classList.remove('active');
-      el.classList.remove('hide');
-    });
-    
-    this.setState(() => ({
-      selected: 'session',
-      sessionLength: this.timer.defaultSession || this.props.sessionLength,
-      breakLength: this.timer.defaultBreak || this.props.breakLength
-    }));
+      document.querySelectorAll("input[type='radio']").forEach(function(el) {
+        el.disabled = false;
+      });
+        
+      document.querySelectorAll(".js-adjust-time").forEach(function(el) {
+        el.disabled = false;
+      });
+      
+      document.querySelectorAll('p').forEach(function(el) {
+        el.classList.remove('active');
+        el.classList.remove('hide');
+      });
+      
+      this.setState(() => ({
+        selected: 'session',
+        sessionLength: this.timer.defaultSession || this.props.sessionLength,
+        breakLength: this.timer.defaultBreak || this.props.breakLength
+      }));
+    }
   }
     
   render() {
@@ -315,7 +207,7 @@ export default class PomodoroClock extends React.Component {
 PomodoroClock.defaultProps = {
   selected: 'session',
   unit: 'min',
-  sessionLength: "6:05",
+  sessionLength: "0:05",
   breakLength: "0:05",
   countDown: null
 };
